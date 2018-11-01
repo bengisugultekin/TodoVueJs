e8cf71edcd4b530faa35f4e800f967574145cc1b
Bengisu Gültekin <bengisugultekin@outlook.com>
2018-11-01 17:20:33 +0300

    Some css changes

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -3,18 +3,21 @@
         <div class="container">
             <h1>Tasks</h1>                  
             <form v-on:submit="addTask">
-                <input type="text" class="add-new-task" v-model="newTask.title" placeholder="Add new task">
-                <input type="submit" value="Add">
+                <input type="text" class="add-new-task" v-model="newTask.title" placeholder="Add new task">           
+                <button class="btn" type="submit">Add</button>
             </form>
             <ul>
                 <li class="new-task-li" v-for="task in tasks"> 
                     <b-form-checkbox v-model="task.completed">
                         <span class="new-task" :class="{completed: task.completed}">
                         {{task.title}}
-                        
-                    </span>
-                        </b-form-checkbox> 
-                        <button v-on:click="deleteTask(task)">x</button>                    
+                        </span>
+                    </b-form-checkbox> 
+                     <button
+                        class="item-remove"
+                        v-on:click="deleteTask(task)">
+                        <font-awesome-icon class="trash" icon="trash" />   
+                     </button>                                
                     <!-- <input type="checkbox" class="toggle" v-model="task.completed"> -->
                     
                 </li>
@@ -34,12 +37,16 @@ export default {
   },
   methods: {
     addTask: function(e) {
-      this.tasks.push({
-        title: this.newTask.title,
-        completed: false
-      });
+      if(this.newTask.title){
+        this.tasks.push({
+          title: this.newTask.title,
+          completed: false
+        });
+        this.newTask.title = "";
+      }      
       e.preventDefault();
     },
+
     deleteTask: function(task) {
       this.tasks.splice(this.tasks.indexOf(task), 1);
     }
@@ -48,6 +55,12 @@ export default {
 </script>
 
 <style scoped>
+.container{
+  margin: auto;
+  position: relative;
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+}
+
 .completed {
   text-decoration: line-through;
 }
@@ -58,19 +71,92 @@ h1 {
   font-size: 80px;
   font-weight: 100;
   text-align: center;
-  color: rgba(175, 47, 47, 0.15);
-}
-
-.add-new-task {
-  width: 80%;
-  max-width: 800px;
+  color: #4fc08d;
 }
 
 .new-task{
     pointer-events: none;
+    width: 100%;    
 }
 
-.new-task-li{
+.item-remove {
+    margin-left: .5em;
+    background: none;
+    border: 1px solid;
+    color: #4fc08d;
+    padding: 0;
+    line-height: 1;
+    width: 2em; 
+    height: 2em;
+    display: initial;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    font-size: 100%;
+  }
+
+li{
     margin-top: 30px;
+    list-style: none;    
 }
+
+ul
+{
+    list-style-type:none;
+    padding:0 0 0 3em;
+    margin:0px;
+}
+
+.trash, .plus{
+  cursor: pointer;
+  color: #4fc08d;
+}
+
+.btn {
+  font-size: 14px;
+  margin: 0 .5em;
+  border-radius: 2em;
+  padding: 0.75em 1.5em;
+  cursor: pointer;
+  background: none;
+  color: darken(#4fc08d, 20%);
+  border: 1px solid;
+  letter-spacing: 1px;
+  /* font-family: $font-family; */
+  color: #4fc08d;
+  border: #4fc08d 1px solid;
+  transition: 250ms ease-out;
+}
+  .btn:hover, .btn:focus {
+    color: #fff;
+    background: #4fc08d;
+  }
+
+
+form {
+  width: 100%;
+  padding: 1.5rem 1rem 0 1rem;
+  display: flex;
+  }
+  form input {
+    width: 100%;
+    font-size: 14px;
+    margin: 0 .5em;
+    border-radius: 2em;
+    padding: 0.75em 1.5em;
+    background: none;
+    /* font-family: $font-family; */
+    border: #e3e3e3 1px solid;
+    transition: border 250ms ease-out;
+    }
+
+    form input:focus {
+      border: #4fc08d 1px solid;
+      outline: none;
+    }
+    
+  
+
+
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,22 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import App from './App'
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import BootstrapVue from 'bootstrap-vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 
-import App from './App'
-
+library.add(faCoffee)
+library.add(faTrash)
+library.add(faPlus)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.use(BootstrapVue);
 Vue.config.productionTip = false
 
