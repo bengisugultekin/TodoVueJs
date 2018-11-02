f22f34b4f7f83e09b818ea8359735244404071ce
Bengisu Gültekin <bengisugultekin@outlook.com>
2018-11-02 16:53:16 +0300

    some css and js

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -14,7 +14,7 @@
                         </span>
                     </b-form-checkbox> 
                      <button
-                        class="item-remove"
+                        class="btn item-remove"
                         v-on:click="deleteTask(task)">
                         <font-awesome-icon class="trash" icon="trash" />   
                      </button>                                
@@ -37,32 +37,43 @@ export default {
   },
   methods: {
     addTask: function(e) {
-      if(this.newTask.title){
+      if (this.newTask.title) {
         this.tasks.push({
           title: this.newTask.title,
           completed: false
         });
         this.newTask.title = "";
-      }      
+      }
       e.preventDefault();
     },
-
     deleteTask: function(task) {
       this.tasks.splice(this.tasks.indexOf(task), 1);
-    }
-  }
+    },
+    mounted(){
+      console.log('App mounted!');
+    }, 
+    watch: {
+      tasks: {
+        handler() { console.log('Todos changed!'); },
+        deep: true,
+      },
+    },
+  },
 };
 </script>
 
+
 <style scoped>
-.container{
+.container {
   margin: auto;
   position: relative;
-  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  font-family: "Lato", "Lucida Grande", "Lucida Sans Unicode", Tahoma,
+    Sans-Serif;
 }
 
 .completed {
   text-decoration: line-through;
+  color: #ccc;
 }
 
 h1 {
@@ -74,47 +85,20 @@ h1 {
   color: #4fc08d;
 }
 
-.new-task{
-    pointer-events: none;
-    width: 100%;    
-}
-
-.item-remove {
-    margin-left: .5em;
-    background: none;
-    border: 1px solid;
-    color: #4fc08d;
-    padding: 0;
-    line-height: 1;
-    width: 2em; 
-    height: 2em;
-    display: initial;
-    align-items: center;
-    justify-content: center;
-    border-radius: 50%;
-    font-size: 100%;
-  }
-
-li{
-    margin-top: 30px;
-    list-style: none;    
-}
-
-ul
-{
-    list-style-type:none;
-    padding:0 0 0 3em;
-    margin:0px;
-}
-
-.trash, .plus{
+.trash,
+.plus {
   cursor: pointer;
   color: #4fc08d;
 }
 
+.trash:hover {
+  color: #fff;
+  background: #4fc08d;
+}
+
 .btn {
   font-size: 14px;
-  margin: 0 .5em;
+  margin: 0 0.5em;
   border-radius: 2em;
   padding: 0.75em 1.5em;
   cursor: pointer;
@@ -122,41 +106,70 @@ ul
   color: darken(#4fc08d, 20%);
   border: 1px solid;
   letter-spacing: 1px;
-  /* font-family: $font-family; */
   color: #4fc08d;
   border: #4fc08d 1px solid;
   transition: 250ms ease-out;
 }
-  .btn:hover, .btn:focus {
-    color: #fff;
-    background: #4fc08d;
-  }
-
+.btn:hover,
+.btn:focus {
+  color: #fff;
+  background: #4fc08d;
+}
 
 form {
   width: 100%;
   padding: 1.5rem 1rem 0 1rem;
   display: flex;
-  }
-  form input {
-    width: 100%;
-    font-size: 14px;
-    margin: 0 .5em;
-    border-radius: 2em;
-    padding: 0.75em 1.5em;
-    background: none;
-    /* font-family: $font-family; */
-    border: #e3e3e3 1px solid;
-    transition: border 250ms ease-out;
-    }
+}
+form input {
+  width: 100%;
+  font-size: 14px;
+  margin: 0 0.5em;
+  border-radius: 2em;
+  padding: 0.75em 1.5em;
+  background: none;
+  border: #e3e3e3 1px solid;
+  transition: border 250ms ease-out;
+}
 
-    form input:focus {
-      border: #4fc08d 1px solid;
-      outline: none;
-    }
-    
-  
+form input:focus {
+  border: #4fc08d 1px solid;
+  outline: none;
+}
 
+li {
+  margin-top: 30px;
+  list-style: none;
+  width: 100%;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0 0 0 3em;
+  margin: 0px;
+  width: 100%;
+}
+
+.new-task {
+  pointer-events: none;
+  width: 100%;
+}
+
+.item-remove {
+  margin-left: 0.5em;
+  background: none;
+  border: 1px solid;
+  color: #4fc08d;
+  padding: 0;
+  line-height: 1;
+  width: 2em;
+  height: 2em;
+  display: initial;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  font-size: 100%;
+}
 
 
 </style>
